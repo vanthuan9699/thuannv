a32acace4c026451edae97b2b97239af6dd25aed
thuanclub <vanthuan080896@gmail.com>
2019-05-13 21:27:20 +0700

    update java

--- SOURCE/app/src/main/java/com/example/androidtest/MainActivity.java
+++ SOURCE/app/src/main/java/com/example/androidtest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.androidtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // ahihi
@@ -9,5 +11,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+
+
+
+
+        // Tạo đối tượng User.
+//      Khai báo + khởi tạo
+        User windsUser = new User();
+//        //cách 1
+//        windsUser.id = 1;
+//        windsUser.phoneNumber = "13546848465";
+//        windsUser.profileUrl = "http://gdkgdgk/dgr.jpg";
+//        windsUser.username = "ThuậnNV";
+
+
+        //cách 2
+        //gán giá trị
+        windsUser.setId(1);
+        windsUser.setPhoneNumber("46542135554");
+        windsUser.setProfileUrl("http:/shfjshjf");
+        windsUser.setUsername("ThuậnNV");
+
+
+//        Toast.makeText(this, windsUser.username, Toast.LENGTH_LONG).show();
+
+//        sử dụng đối tượng mà được khai báo và khởi tạo trong XML
+//        bước 1: gán id cho đối tượng trong XML
+//        bước 2: tạo tên mới và gắn lại cho đối tượng đó
+
+        EditText edtPhoneNumber = findViewById(R.id.edt_phone_number);
+        edtPhoneNumber.setText("Hello phonenumber");
+        edtPhoneNumber.setTextSize(25);
+
     }
 }
---
+++ SOURCE/app/src/main/java/com/example/androidtest/User.java
@@ -0,0 +1,38 @@
+package com.example.androidtest;
+
+public class User {
+    int id;
+    String username,phoneNumber,profileUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+}
